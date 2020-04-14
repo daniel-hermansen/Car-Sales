c76399d1fa8242d868e1eca51bc33ab8393f9ec0
Daniel Hermansen <daniel.j.hermansen@gmail.com>
2020-04-13 20:37:43 -0600

    Added redux store. Added actions and reducers and finised MVP requirements in component files.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
+import { connect } from "react-redux";
+import { addFeature, removeItem } from "./actions/actions";
 
-const App = () => {
+const App = ({ state, addFeature, removeItem }) => {
+  
+
+  
   const state = {
     additionalPrice: 0,
     car: {
@@ -25,24 +29,35 @@ const App = () => {
 
   const removeFeature = item => {
     // dispatch an action here to remove an item
+    removeItem(item)
   };
 
   const buyItem = item => {
     // dipsatch an action here to add an item
+    addFeature(item)
   };
 
   return (
     <div className="boxes">
       <div className="box">
         <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <AddedFeatures car={state.car} removeFeature={removeFeature} />
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
+        <AdditionalFeatures store={state.store} buyItem={buyItem}/>
         <Total car={state.car} additionalPrice={state.additionalPrice} />
       </div>
     </div>
   );
 };
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    state: state
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { addFeature, removeItem }
+)(App);
---
+++ src/actions/actions.js
@@ -0,0 +1,13 @@
+
+export const ADD_FEATURE = "ADD_FEATURE";
+export const REMOVE_FEATURE = "REMOVE_FEATURE";
+
+export const addFeature = feature => {
+    console.log("Add Feature");
+    return { type: ADD_FEATURE, payload: feature };
+};
+
+export const removeItem = feature => {
+    console.log("Remove Feature");
+    return { type: REMOVE_FEATURE, payload: feature };
+};
